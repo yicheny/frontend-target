f8e7aced1180d1b6f8c6b42b768385427cc0bc3b
yanglvfeng <yanglvfeng@croot.com>
2022-01-19 19:17:59 +0800

    update

--- language/C++/C++Primer Version5/codes/ch7/extra_params.cpp
+++ language/C++/C++Primer Version5/codes/ch7/extra_params.cpp
@@ -12,23 +12,25 @@ vector<int>::const_iterator find_val(
     auto res_iter = end;
     occurs = 0;
     for(;beg != end;++beg){
-        if(*beg == value)
+        if(*beg == value){
             res_iter = beg;
-        ++occurs;
+            ++occurs;
+        }
     }
     return res_iter;
 }
 
 int main() {
     vector<int> ivec;
-    ivec.reserve(10);//分配最小存储长度
+    ivec.reserve(11);//分配最小存储长度
     for(int i=0;i<10;i++){
-        ivec.push_back(i);
+        ivec.push_back(10-i);
     }
+    ivec.push_back(3);
     size_t ctr;
 
-//    auto result = find_val(ivec.begin(),ivec.end(),3,ctr);
-
-    std::cout << ivec[5] << std::endl;
+    auto result = find_val(ivec.begin(),ivec.end(),3,ctr);
+    std::cout << *result << std::endl;
+    std::cout << ctr << std::endl;
     return 0;
 }
