84705613a77f2b45a4bdf419eb4df8461d15c68c
yanglvfeng <ylvfeng@gmail.com>
2022-02-25 16:36:34 +0800

    update 《数据结构 C++版》 第4章 栈和队列

--- base/algorithm/数据结构C++邓俊辉/codes/ch4/stack.test.cpp
+++ base/algorithm/数据结构C++邓俊辉/codes/ch4/stack.test.cpp
@@ -1,6 +1,7 @@
 #include "tools/utils/print.h"
 #include "tools/DS/stack.h"
 #include "tools/utils/convert.h"
+#include "tools/utils/paren.h"
 
 int main() {
     system("chcp 65001");
@@ -27,5 +28,17 @@ int main() {
     Stack<char> stack1;
     convert(stack1, 16665, 16);
     stack1.traverse(print);
+
+    print("-----测试match()-----");
+    char chars[] = "123()1()2((3+4))890";
+    int size = sizeof chars / sizeof *chars - 1;
+    print(size);
+    print(match(chars,0,size));
+
+    print("-----测试paren()-----");
+    print(paren(chars,0,size));
+
+    print("-----测试paren_iterate()-----");
+    print(paren_iterate(chars,0,size));
     return 0;
 }
