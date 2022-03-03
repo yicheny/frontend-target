220b322ba26dd22b8ff82d45806f2631d18d2afa
yanglvfeng <ylvfeng@gmail.com>
2022-03-03 18:37:37 +0800

    update 《数据结构 C++版》 二叉树

--- base/algorithm/数据结构C++邓俊辉/codes/ch4/queue.h
+++ base/algorithm/数据结构C++邓俊辉/codes/ch4/queue.h
@@ -1,6 +1,28 @@
 #ifndef C___PROJECT_QUEUE_H
 #define C___PROJECT_QUEUE_H
 
+
+#include "./list.h"
+
+//基于List实现
+template<typename T>
+class Queue : public List<T> {
+public:
+    void enqueue(T const &e) {
+        this->insertAsLast(e);
+    }
+
+    T dequeue() {
+        return this->remove(this->first());
+    }
+
+    T &front() {
+        return this->first()->data;
+    }
+};
+
+//基于Vector实现
+/*
 #include "./vector.h"
 
 template<typename T>
@@ -18,5 +40,6 @@ public:
         return (*this)[0];
     }
 };
+*/
 
 #endif //C___PROJECT_QUEUE_H
